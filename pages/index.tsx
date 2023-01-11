8bd01149a15352561f33710ed779ab59416a7bbd
“Jeffrey <jay.zalischi@gmail.com>
2023-01-11 12:25:34 -0500

    updated data and colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,51 @@
+import Head from 'next/head';
 import Image from 'next/image';
 import { useEffect } from 'react';
 import data from '../data.json';
 
 export default function Home() {
 	return (
-		<div className='flex items-center flex-col mx-auto w-full justify-center mt-16 px-8'>
-			<Image
-				className='rounded-full'
-				alt={data.name}
-				src={data.avatar}
-				width={96}
-				height={96}
-			/>
-			<h1 className='font-bold mt-4 mb-8 text-xl text-white'>{data.name}</h1>
-			{data.links.map((link) => {
-				return <LinkCard key={link.title} {...link} />;
-			})}
-			<div className='social-container flex items-center gap-4 mt-8 text-white'>
-				{data.socials.map((social) => (
-					<a
-						aria-label={`${social.title} link`}
-						key={social.href}
-						href={social.href}
-						target='_blank'
-						rel='noopener noreferrer'
-					>
-						{social.href.includes('linkedin') ? (
-							<LinkedinIcon />
-						) : social.href.includes('github') ? (
-							<GitHubIcon />
-						) : null}
-					</a>
-				))}
+		<>
+			<Head>
+				<meta name='theme-color' content='#00008B' />
+				<meta
+					name='description'
+					content='Linktree Clone built with Next.js and Tailwind'
+				/>
+				<title>Jeffrey Zalischi | Projects</title>
+			</Head>
+			<div className='flex items-center flex-col mx-auto w-full justify-center mt-16 px-8'>
+				<Image
+					className='rounded-full'
+					alt={data.name}
+					src={data.avatar}
+					width={96}
+					height={96}
+				/>
+
+				<h1 className='font-bold mt-4 mb-8 text-xl text-white'>{data.name}</h1>
+				{data.links.map((link) => {
+					return <LinkCard key={link.title} {...link} />;
+				})}
+				<div className='social-container flex items-center gap-4 mt-8 text-white'>
+					{data.socials.map((social) => (
+						<a
+							aria-label={`${social.title} link`}
+							key={social.href}
+							href={social.href}
+							target='_blank'
+							rel='noopener noreferrer'
+						>
+							{social.href.includes('linkedin') ? (
+								<LinkedinIcon />
+							) : social.href.includes('github') ? (
+								<GitHubIcon />
+							) : null}
+						</a>
+					))}
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
